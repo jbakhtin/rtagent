c063d2e5a00925d55bd0147882b4d9281333f1fd
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-02-04 20:12:56 +0700

    Changes: - move some parts of context into Monitorng structure - incapsulate Done logick

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -21,46 +21,55 @@ type Monitor struct {
 
 	memStats runtime.MemStats
 
-	ctx context.Context
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
-func (monitor *Monitor) pool(ctx context.Context) {
+func (monitor *Monitor) pool() {
 	ticker := time.NewTicker(monitor.pollInterval)
 
 	for {
 		select {
 			case <-ticker.C:
 				fmt.Println("Метрика собрана")
-			case <-ctx.Done():
+			case <-monitor.ctx.Done():
 				fmt.Println("Сбор метрик приостановлен!")
 				return
 		}
 	}
 }
 
-func (monitor *Monitor) report(ctx context.Context) {
+func (monitor *Monitor) report() {
 	ticker := time.NewTicker(monitor.reportInterval)
 
 	for {
 		select {
 			case <-ticker.C:
 				fmt.Println("Метрика отрпавлена!")
-			case <-ctx.Done():
+			case <-monitor.ctx.Done():
 				fmt.Println("Отправка метрики приостановлена!")
 				return
 		}
 	}
 }
 
-func (monitor *Monitor) Start (ctx context.Context) {
-	go monitor.pool(ctx)
-	go monitor.report(ctx)
+func (monitor *Monitor) Start () {
+	go monitor.pool()
+	go monitor.report()
 }
 
-func NewMonitor(pollInterval, reportInterval time.Duration) Monitor {
+func (monitor *Monitor) Done () {
+	monitor.cancel()
+}
+
+func NewMonitor(ctx context.Context, pollInterval, reportInterval time.Duration) Monitor {
+	ctx, cancel := context.WithCancel(ctx)
+
 	return Monitor{
-		pollInterval: pollInterval,
+		pollInterval:   pollInterval,
 		reportInterval: reportInterval,
+		ctx:            ctx,
+		cancel:         cancel,
 	}
 }
 
@@ -70,12 +79,11 @@ func NewMonitor(pollInterval, reportInterval time.Duration) Monitor {
 
 func main() {
 	ctx := context.Background()
-	ctx, cencel := context.WithCancel(ctx)
 
-	monitor := NewMonitor(time.Second * 2, time.Second * 10)
-	monitor.Start(ctx)
+	monitor := NewMonitor(ctx, time.Second * 2, time.Second * 10)
 
-	time.AfterFunc(time.Second * 20, cencel)
+	monitor.Start()
+	time.AfterFunc(time.Second * 20, monitor.Done)
 
 	timer := time.NewTimer(time.Second * 60)
 	<-timer.C
