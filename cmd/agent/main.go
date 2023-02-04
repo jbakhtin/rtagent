33ba6b2ea88a7381ac31b85f76d17e837e46c239
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-02-04 19:57:45 +0700

    Changes: - implemen stop monitoring with Context instead chanel

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"runtime"
 	"time"
@@ -20,51 +21,46 @@ type Monitor struct {
 
 	memStats runtime.MemStats
 
-	stop chan bool
+	ctx context.Context
 }
 
-func (monitor *Monitor) pool() {
+func (monitor *Monitor) pool(ctx context.Context) {
 	ticker := time.NewTicker(monitor.pollInterval)
 
 	for {
 		select {
 			case <-ticker.C:
 				fmt.Println("Метрика собрана")
-			case <-monitor.stop:
+			case <-ctx.Done():
 				fmt.Println("Сбор метрик приостановлен!")
 				return
 		}
 	}
 }
 
-func (monitor *Monitor) report() {
+func (monitor *Monitor) report(ctx context.Context) {
 	ticker := time.NewTicker(monitor.reportInterval)
 
 	for {
 		select {
 			case <-ticker.C:
 				fmt.Println("Метрика отрпавлена!")
-			case <-monitor.stop:
+			case <-ctx.Done():
 				fmt.Println("Отправка метрики приостановлена!")
 				return
 		}
 	}
 }
 
-func (monitor *Monitor) Start () {
-	go monitor.pool()
-	go monitor.report()
-}
-
-func (monitor *Monitor) Stop () {
-	monitor.stop <- true
+func (monitor *Monitor) Start (ctx context.Context) {
+	go monitor.pool(ctx)
+	go monitor.report(ctx)
 }
 
 func NewMonitor(pollInterval, reportInterval time.Duration) Monitor {
 	return Monitor{
 		pollInterval: pollInterval,
 		reportInterval: reportInterval,
-		stop: make(chan bool, 0),
 	}
 }
 
@@ -73,20 +69,14 @@ func NewMonitor(pollInterval, reportInterval time.Duration) Monitor {
 //TODO: сделать остановшик на опредленоое время
 
 func main() {
+	ctx := context.Background()
+	ctx, cencel := context.WithCancel(ctx)
+
 	monitor := NewMonitor(time.Second * 2, time.Second * 10)
-	monitor.Start()
+	monitor.Start(ctx)
 
-	//monitor.Stop()
+	time.AfterFunc(time.Second * 20, cencel)
 
-	timer := time.NewTimer(time.Second * 15)
-	timer2 := time.NewTimer(time.Second * 20)
-
-	select {
-		case <-timer.C:
-			fmt.Println("Сбор данных приостановлен!")
-			monitor.Stop()
-			monitor.Stop()
-	}
-
-	<-timer2.C
+	timer := time.NewTimer(time.Second * 60)
+	<-timer.C
 }
