9158e0a0ee7fd07612c1abbec12a9bcc644f0cf7
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-02-28 02:17:05 +0700

    Changes: - fixed set adderess for agent

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -32,7 +32,7 @@ type Monitor struct {
 func New(ctf config.Config, logger *zap.Logger) (Monitor, error){
 	return Monitor{
 		log: logger,
-		serverAddress: ctf.Address,
+		serverAddress: fmt.Sprintf("http://%s", ctf.Address),
 		pollInterval: ctf.PollInterval,
 		reportInterval: ctf.ReportInterval,
 	}, nil
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,7 @@ package config
 import "time"
 
 type Config struct {
-	PollInterval   time.Duration `env:"POLL_INTERVAL"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-	Address        string        `env:"ADDRESS"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
+	Address        string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
 }
