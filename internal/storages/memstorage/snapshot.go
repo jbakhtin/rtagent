d5a48d24873defb5ec7b76c555c625362e6bbb2a
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-02-28 22:07:05 +0700

    Features: - added library for work with filesystem

---
+++ internal/storages/memstorage/snapshot.go
@@ -0,0 +1,73 @@
+package memstorage
+
+import (
+	"context"
+	"fmt"
+	"github.com/jbakhtin/rtagent/internal/config"
+	"github.com/jbakhtin/rtagent/internal/models"
+	"time"
+)
+
+type Snapshot struct {
+	ToFile *toFile
+	FromFile *fromFile
+
+	metrics *map[string]models.Metricer
+}
+
+func NewSnapshot(ctx context.Context, cfg config.Config) (*Snapshot, error) {
+	newReader, err := NewReader(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	newWriter, err := NewWriter(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Snapshot{
+		FromFile: newReader,
+		ToFile: newWriter,
+	}, nil
+}
+
+func (s *Snapshot) Import(ctx context.Context) (map[string]models.Metric, bool) {
+	list, err := s.FromFile.ReadList()
+	if err != nil {
+		return nil, false
+	}
+
+	return list, true
+}
+
+func (s *Snapshot) Exporting(ctx context.Context, cfg config.Config, metrics *map[string]models.Metric) {
+	ticker := time.NewTicker(cfg.StoreInterval)
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("test")
+			err := s.ToFile.WriteList(metrics)
+			if err != nil {
+				return
+			}
+			return
+		case <-ticker.C:
+			err := s.ToFile.WriteList(metrics)
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+
+func (s *Snapshot) Export(ctx context.Context, metrics map[string]models.Metric) error {
+	err := s.ToFile.WriteList(&metrics)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/storages/memstorage/tofile.go
@@ -0,0 +1,68 @@
+package memstorage
+
+import (
+	"bufio"
+	"encoding/json"
+	"github.com/jbakhtin/rtagent/internal/config"
+	"github.com/jbakhtin/rtagent/internal/models"
+	"os"
+)
+
+type Writer interface {
+	Write(event *models.Metric) // для записи события
+	Close() error            // для закрытия ресурса (файла)
+}
+
+type toFile struct {
+	file *os.File // файл для записи
+	writer *bufio.Writer
+}
+
+func NewWriter(cfg config.Config) (*toFile, error) {
+	file, err := os.OpenFile(cfg.StoreFile, os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+		return nil, err
+	}
+
+	return &toFile{
+		file: file,
+		// создаём новый Writer
+		writer: bufio.NewWriter(file),
+	}, nil
+}
+
+func (tf *toFile) WriteList(event *map[string]models.Metric) error {
+	data, err := json.Marshal(&event)
+	if err != nil {
+		return err
+	}
+
+	// записываем событие в буфер
+	if _, err := tf.writer.Write(data); err != nil {
+		return err
+	}
+
+	// добавляем перенос строки
+	err = tf.file.Truncate(0)
+	if err != nil {
+		return err
+	}
+	_, err = tf.file.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+	if err = tf.writer.WriteByte('\n'); err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	// записываем буфер в файл
+	return tf.writer.Flush()
+}
+
+func (tf *toFile) Close() error {
+	// закрываем файл
+	//return tf.file.Close()
+	return nil
+}
