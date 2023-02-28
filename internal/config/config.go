f95f8faaa4c05e0fafc9c249d36c9bcbc14767b8
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-02-28 22:48:25 +0700

    Test

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,7 +6,7 @@ type Config struct {
 	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
 	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
 	Address        string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
-	StoreFile      string        `env:"STORE_FILE" envDefault:"tmp/devops-metrics-db.json"`
+	StoreFile      string        `env:"STORE_FILE" envDefault:"devops-metrics-db.json"`
 	StoreInterval  time.Duration `env:"STORE_INTERVAL" envDefault:"40s"`
 	Restore        bool          `env:"RESTORE" envDefault:"true"`
 }
