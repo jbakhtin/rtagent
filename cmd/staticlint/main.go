76aa881571bcd39083fe234ac0a52a75aa18f885
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-08-29 18:59:05 +0700

    Features: - added first version of multichecker

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/ifaceassert"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"honnef.co/go/tools/staticcheck"
+	"os"
+)
+
+var StaticCheckPath = "multichecker_config.json"
+type Config struct {
+	Staticcheck []string
+}
+
+func main() {
+	data, err := os.ReadFile(StaticCheckPath)
+	if err != nil {
+		panic(err)
+	}
+
+	var cfg Config
+	if err = json.Unmarshal(data, &cfg); err != nil {
+		panic(err)
+	}
+
+	mychecks := []*analysis.Analyzer{
+		printf.Analyzer,
+		shadow.Analyzer,
+		shift.Analyzer,
+		structtag.Analyzer,
+		copylock.Analyzer,
+		httpresponse.Analyzer,
+		ifaceassert.Analyzer,
+		loopclosure.Analyzer,
+		tests.Analyzer,
+		unreachable.Analyzer,
+	}
+	checks := make(map[string]bool)
+	for _, v := range cfg.Staticcheck {
+		checks[v] = true
+	}
+	// добавляем анализаторы из staticcheck, которые указаны в файле конфигурации
+	for _, v := range staticcheck.Analyzers {
+		if checks[v.Analyzer.Name] {
+			mychecks = append(mychecks, v.Analyzer)
+		}
+	}
+	multichecker.Main(
+		mychecks...,
+	)
+}
--- go.mod
+++ go.mod
@@ -5,14 +5,14 @@ go 1.19
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.0.8
-	github.com/go-resty/resty/v2 v2.7.0
 	github.com/jackc/pgx/v5 v5.3.1
 	github.com/pressly/goose/v3 v3.10.0
 	github.com/shirou/gopsutil/v3 v3.23.3
 	github.com/stretchr/testify v1.8.2
 	go.uber.org/zap v1.24.0
 	golang.org/x/exp v0.0.0-20230210204819-062eb4c674ab
-	golang.org/x/sync v0.1.0
+	golang.org/x/tools v0.9.4-0.20230601214343-86c93e8732cc
+	honnef.co/go/tools v0.4.5
 )
 
 require (
@@ -31,8 +31,9 @@ require (
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
