b4b0e7fd8f173260cb429e6524b3b7b134e09571
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2023-09-05 18:34:49 +0700

    Changes: - changed test

--- internal/server/handlers/metric_test.go
+++ internal/server/handlers/metric_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestHandlerMetric_Get(t *testing.T) {
-	//type fields struct {
-	//	repo    MetricRepository
-	//	request string
-	//}
-	//type want struct {
-	//	statusCode int
-	//}
-	//
+	type fields struct {
+		repo    MetricRepository
+		request string
+	}
+	type want struct {
+		statusCode int
+	}
+
 	//ctx := context.TODO()
 	//cfg, err := config.NewConfigBuilder().
 	//	WithAllFromFlagsS().
@@ -27,38 +27,38 @@ func TestHandlerMetric_Get(t *testing.T) {
 	//	fmt.Println(err)
 	//	return
 	//}
-	//
-	//tests := []struct {
-	//	name   string
-	//	fields fields
-	//	want   want
-	//}{
-	//	{
-	//		"Get all metrics. Test 1",
-	//		fields{
-	//			repo:    &storage,
-	//			request: "http://127.0.0.1:8080",
-	//		},
-	//		want{
-	//			statusCode: 200,
-	//		},
-	//	},
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		h, err := NewHandlerMetric(ctx, cfg)
-	//		if err != nil {
-	//			require.NoError(t, err)
-	//		}
-	//
-	//		request := httptest.NewRequest(http.MethodGet, tt.fields.request, nil)
-	//		w := httptest.NewRecorder()
-	//		hf := h.GetAllMetricsAsHTML()
-	//		hf(w, request)
-	//		result := w.Result()
-	//		assert.Equal(t, tt.want.statusCode, result.StatusCode)
-	//		err = result.Body.Close()
-	//		require.NoError(t, err)
-	//	})
-	//}
+
+	tests := []struct {
+		name   string
+		fields fields
+		want   want
+	}{
+		{
+			"Get all metrics. Test 1",
+			fields{
+				//repo:    &storage,
+				request: "http://127.0.0.1:8080",
+			},
+			want{
+				statusCode: 200,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//h, err := NewHandlerMetric(ctx, cfg)
+			//if err != nil {
+			//	require.NoError(t, err)
+			//}
+			//
+			//request := httptest.NewRequest(http.MethodGet, tt.fields.request, nil)
+			//w := httptest.NewRecorder()
+			//hf := h.GetAllMetricsAsHTML()
+			//hf(w, request)
+			//result := w.Result()
+			//assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			//err = result.Body.Close()
+			//require.NoError(t, err)
+		})
+	}
 }
